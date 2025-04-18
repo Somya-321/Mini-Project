f23c5c4927073947997e60e5c1e17690747f83a6
Somya-321 <somyamaurya001@gmail.com>
2025-04-18 10:17:50 +0530

    first commit

---
+++ forging.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const video = document.getElementById("backgroundVideo");
+
+//     if (video) {
+//         video.playbackRate = 0.5; // Slow down video playback speed
+//         console.log("Playback speed set to", video.playbackRate); // Debugging check
+//     } else {
+//         console.error("Video element not found");
+//     }
+// });
